c9d43a5d74e52d5c92f628cac21e4f1a4960bafe
Pierre-Alexandre Meyer <pierre@mouraf.org>
2014-11-18 07:44:31 -0500

    docker: add support for plugins
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- docker/killbill.sh
+++ docker/killbill.sh
@@ -22,9 +22,9 @@ require 'yaml'
 
 raw_kpm = File.new("#{ENV['KILLBILL_CONFIG']}/kpm.yml.erb").read
 parsed_kpm = ERB.new(raw_kpm).result
-yml_kpm = YAML.load(parsed_kpm)['killbill']
+yml_kpm = YAML.load(parsed_kpm)
 
-properties = yml_kpm['properties']
+properties = yml_kpm['killbill']['properties']
 File.open("#{ENV['KILLBILL_CONFIG']}/killbill.properties", 'w') do |file|
   properties.each do |key, value|
     file.write("#{key}=#{value}\n")
@@ -37,13 +37,36 @@ EORUBY
     echo >&2 "Kill Bill kpm file not found - creating now..."
     jruby <<-EORUBY
 require 'erb'
+require 'kpm'
 require 'yaml'
 
 raw_kpm = File.new("#{ENV['KILLBILL_CONFIG']}/kpm.yml.erb").read
 parsed_kpm = ERB.new(raw_kpm).result
 
+yml_kpm = YAML.load(parsed_kpm)
+yml_kpm['killbill']['plugins'] ||= {}
+yml_kpm['killbill']['plugins']['java'] ||= []
+yml_kpm['killbill']['plugins']['ruby'] ||= []
+
+plugin_configurations = ENV.select { |key, value| key.to_s.match(/^KILLBILL_PLUGIN_/) }
+plugin_configurations.each do |key, value|
+  # key is for example KILLBILL_PLUGIN_STRIPE
+  plugin = key.match(/^KILLBILL_PLUGIN_(.*)/).captures.first
+
+  # Do we know about it?
+  group_id, artifact_id, packaging, classifier, version, type = KPM::PluginsDirectory.lookup(plugin, true)
+  next if group_id.nil?
+
+  # Plugin already present?
+  matches = yml_kpm['killbill']['plugins'][type.to_s].select { |p| p['artifact_id'] == artifact_id || p['name'] == artifact_id }
+  next unless matches.empty?
+
+  yml_kpm['killbill']['plugins'][type.to_s] << [{'group_id' => group_id, 'artifact_id' => artifact_id, 'packaging' => packaging, 'classifier' => classifier, 'version' => version}]
+end
+final_kpm = yml_kpm.to_yaml
+
 File.open("#{ENV['KILLBILL_CONFIG']}/kpm.yml", 'w') do |file|
-  file.write(parsed_kpm)
+  file.write(final_kpm)
 end
 EORUBY
 
