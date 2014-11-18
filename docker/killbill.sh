0e7dc8b8d2f78a7b436b2c98b31c26209273fa74
Pierre-Alexandre Meyer <pierre@mouraf.org>
2014-11-18 05:36:46 -0500

    docker: various fixes
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- docker/killbill.sh
+++ docker/killbill.sh
@@ -1,26 +1,41 @@
+#!/bin/bash
+
 set -a
 
-if ! [ -e $KILLBILL_CONFIG/killbill.properties ]; then
-  echo >&2 "Kill Bill properties file not found - creating now..."
-  jruby <<-EORUBY
+source "/etc/default/tomcat7"
+
+JAVA_HOME="/usr/lib/jvm/default-java"
+CATALINA_HOME="/usr/share/tomcat7"
+CATALINA_BASE="/var/lib/tomcat7"
+JAVA_OPTS="-Djava.awt.headless=true"
+CATALINA_PID="/var/run/tomcat7.pid"
+CATALINA_TMPDIR="/tmp/tomcat7-tomcat7-tmp"
+LANG=""
+JSSE_HOME="/usr/lib/jvm/default-java/jre/"
+
+function install {
+  if ! [ -e $KILLBILL_CONFIG/killbill.properties ]; then
+    echo >&2 "Kill Bill properties file not found - creating now..."
+    jruby <<-EORUBY
 require 'erb'
 require 'yaml'
 
 raw_kpm = File.new("#{ENV['KILLBILL_CONFIG']}/kpm.yml.erb").read
 parsed_kpm = ERB.new(raw_kpm).result
-properties = YAML.load(parsed_kpm)['killbill']['properties']
+yml_kpm = YAML.load(parsed_kpm)['killbill']
 
+properties = yml_kpm['properties']
 File.open("#{ENV['KILLBILL_CONFIG']}/killbill.properties", 'w') do |file|
   properties.each do |key, value|
     file.write("#{key}=#{value}\n")
   end
 end
 EORUBY
-fi
+  fi
 
-if ! [ -e $KILLBILL_CONFIG/kpm.yml ]; then
-  echo >&2 "Kill Bill kpm file not found - creating now..."
-  jruby <<-EORUBY
+  if ! [ -e $KILLBILL_CONFIG/kpm.yml ]; then
+    echo >&2 "Kill Bill kpm file not found - creating now..."
+    jruby <<-EORUBY
 require 'erb'
 require 'yaml'
 
@@ -32,20 +47,35 @@ File.open("#{ENV['KILLBILL_CONFIG']}/kpm.yml", 'w') do |file|
 end
 EORUBY
 
-  echo >&2 "Starting Kill Bill installation..."
-  jruby -S kpm install $KILLBILL_CONFIG/kpm.yml
-fi
+    echo >&2 "Starting Kill Bill installation..."
+    jruby -S kpm install $KILLBILL_CONFIG/kpm.yml
+  fi
+}
 
-source "/etc/default/tomcat7"
+function run {
+  install
 
-JAVA_HOME="/usr/lib/jvm/default-java"
-CATALINA_HOME="/usr/share/tomcat7"
-CATALINA_BASE="/var/lib/tomcat7"
-JAVA_OPTS="-Djava.awt.headless=true -Xmx128m -XX:+UseConcMarkSweepGC"
-CATALINA_PID="/var/run/tomcat7.pid"
-CATALINA_TMPDIR="/tmp/tomcat7-tomcat7-tmp"
-LANG=""
-JSSE_HOME="/usr/lib/jvm/default-java/jre/"
+  # Load JVM properties
+  export CATALINA_OPTS=$(jruby -rerb -ryaml -e 'puts YAML.load(ERB.new(File.new("#{ENV['"'KILLBILL_CONFIG'"']}/kpm.yml.erb").read).result)["killbill"]["jvm"]')
 
-cd /var/lib/tomcat7
-/usr/share/tomcat7/bin/catalina.sh run
+  cd /var/lib/tomcat7 && /usr/share/tomcat7/bin/catalina.sh run
+}
+
+function reinstall {
+  rm -f $KILLBILL_CONFIG/killbill.properties \
+        $KILLBILL_CONFIG/kpm.yml \
+        $TOMCAT_HOME/logs/*
+  install
+}
+
+case "$1" in
+  run)
+    run
+    ;;
+  reinstall)
+    reinstall
+    ;;
+  *)
+    echo $"Usage: $0 {run|reinstall}"
+    exit 1
+esac
