8b0031a2fb0aa377f50582ac8c96fdc9f7b3b0fa
stephane brossier <sbrossier@groupon.com>
2014-12-01 12:15:55 -0800

    Fix cleanup of temporary Dockerfile when building tagged image

--- docker/dockerTemplate.sh
+++ docker/dockerTemplate.sh
@@ -2,8 +2,37 @@
 
 TAGGED_PATH="templates/tagged"
 
-if [[ $VERSION != 'LATEST' ]]; then
-    rm -f "$TAGGED_PATH/Dockerfile"
+ACTION=
+
+help() {
+    echo "dockerTemplate.sh -i (init) -c (clean) -v THE_VERSION"
+    exit 1
+}
+
+init() {
+  if [[ $VERSION != 'LATEST' ]]; then
+    clean
     cat "$TAGGED_PATH/Dockerfile.template"  | sed -e "s/__VERSION__/$VERSION/" > "$TAGGED_PATH/Dockerfile"
+  fi
+}
+
+clean() {
     rm -f "$TAGGED_PATH/Dockerfile"
+}
+
+while getopts "hicv:" OPTION; do
+  case $OPTION in
+    h) help;;
+    i) ACTION="INIT";;
+    c) ACTION="CLEAN";;
+    v) VERSION="$OPTARG";;
+  esac
+done
+
+if [[ $ACTION == "INIT" ]]; then
+  init
+elif [[ $ACTION == "CLEAN" ]]; then
+  clean
+else
+  help
 fi
