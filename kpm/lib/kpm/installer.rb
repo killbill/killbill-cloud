4b5a6be3aaae29e057f24a0a9096a49d1d959885
Pierre-Alexandre Meyer <pierre@mouraf.org>
2016-04-26 11:04:22 -0700

    kpm: fix NPE in installer
    This fixes https://github.com/killbill/killbill-cloud/issues/52.
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- kpm/lib/kpm/installer.rb
+++ kpm/lib/kpm/installer.rb
@@ -31,15 +31,17 @@ module KPM
     end
 
     def install(force_download=false, verify_sha1=true)
+      bundles_dir = @config['plugins_dir'] || DEFAULT_BUNDLES_DIR
+
       help = nil
       unless @config.nil?
         help = install_tomcat if @config['webapp_path'].nil?
         install_killbill_server(@config['group_id'], @config['artifact_id'], @config['packaging'], @config['classifier'], @config['version'], @config['webapp_path'], force_download, verify_sha1)
-        install_plugins(force_download, verify_sha1)
+        install_plugins(bundles_dir, force_download, verify_sha1)
         unless @config['default_bundles'] == false
-          install_default_bundles(@config['plugins_dir'], @config['default_bundles_version'], @config['version'], force_download, verify_sha1)
+          install_default_bundles(bundles_dir, @config['default_bundles_version'], @config['version'], force_download, verify_sha1)
         end
-        clean_up_descriptors
+        clean_up_descriptors(bundles_dir)
       end
 
       unless @kaui_config.nil?
@@ -72,45 +74,45 @@ module KPM
       manager.help
     end
 
-    def install_plugins(force_download, verify_sha1)
-      install_java_plugins(force_download, verify_sha1)
-      install_ruby_plugins(force_download, verify_sha1)
+    def install_plugins(bundles_dir, force_download, verify_sha1)
+      install_java_plugins(bundles_dir, force_download, verify_sha1)
+      install_ruby_plugins(bundles_dir, force_download, verify_sha1)
     end
 
-    def install_java_plugins(force_download, verify_sha1)
+    def install_java_plugins(bundles_dir, force_download, verify_sha1)
       return if @config['plugins'].nil? or @config['plugins']['java'].nil?
 
       infos = []
       @config['plugins']['java'].each do |plugin|
-        infos << install_plugin(plugin['name'], nil, plugin['group_id'], plugin['artifact_id'], plugin['packaging'], plugin['classifier'], plugin['version'], @config['plugins_dir'], 'java', force_download, verify_sha1, false)
+        infos << install_plugin(plugin['name'], nil, plugin['group_id'], plugin['artifact_id'], plugin['packaging'], plugin['classifier'], plugin['version'], bundles_dir, 'java', force_download, verify_sha1, false)
       end
 
       infos
     end
 
-    def install_ruby_plugins(force_download, verify_sha1)
+    def install_ruby_plugins(bundles_dir, force_download, verify_sha1)
       return if @config['plugins'].nil? or @config['plugins']['ruby'].nil?
 
       verify_jruby_jar=true
       infos = []
       @config['plugins']['ruby'].each do |plugin|
-        infos << install_plugin(plugin['name'], nil, plugin['group_id'], plugin['artifact_id'], plugin['packaging'], plugin['classifier'], plugin['version'], @config['plugins_dir'], 'ruby', force_download, verify_sha1, verify_jruby_jar)
+        infos << install_plugin(plugin['name'], nil, plugin['group_id'], plugin['artifact_id'], plugin['packaging'], plugin['classifier'], plugin['version'], bundles_dir, 'ruby', force_download, verify_sha1, verify_jruby_jar)
         verify_jruby_jar=false
       end
 
       infos
     end
 
-    def clean_up_descriptors
-      removed_plugins = clean_up_plugin_identifiers
-      clean_up_sha1s(removed_plugins)
+    def clean_up_descriptors(bundles_dir)
+      removed_plugins = clean_up_plugin_identifiers(bundles_dir)
+      clean_up_sha1s(removed_plugins, bundles_dir)
     end
 
-    def clean_up_plugin_identifiers
+    def clean_up_plugin_identifiers(bundles_dir)
       inspector = KPM::Inspector.new
-      installed_plugins = inspector.inspect(@config['plugins_dir'])
+      installed_plugins = inspector.inspect(bundles_dir)
 
-      plugins_installation_path = File.join(@config['plugins_dir'], 'plugins')
+      plugins_installation_path = File.join(bundles_dir, 'plugins')
       plugins_manager = KPM::PluginsManager.new(plugins_installation_path, @logger)
 
       plugin_identifiers = plugins_manager.read_plugin_identifiers
@@ -126,8 +128,8 @@ module KPM
       removed_identifiers
     end
 
-    def clean_up_sha1s(removed_plugins)
-      sha1checker = KPM::Sha1Checker.from_file(File.join(@config['plugins_dir'], KPM::BaseInstaller::SHA1_FILENAME))
+    def clean_up_sha1s(removed_plugins, plugins_dir)
+      sha1checker = KPM::Sha1Checker.from_file(File.join(plugins_dir, KPM::BaseInstaller::SHA1_FILENAME))
       removed_plugins.each do |removed|
         coordinates = KPM::Coordinates.build_coordinates(group_id: removed['group_id'],
                                                          artifact_id: removed['artifact_id'],
