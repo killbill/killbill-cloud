10839be3754081f51115865fc3efa4585556acab
stephane brossier <sbrossier@groupon.com>
2016-05-16 20:02:15 -0700

    Fix broekn migration spec

--- kpm/spec/kpm/remote/migrations_spec.rb
+++ kpm/spec/kpm/remote/migrations_spec.rb
@@ -4,7 +4,7 @@ describe KPM::Migrations do
 
   context 'plugins' do
     it 'should be able to find migrations for a java plugin' do
-      migrations = KPM::Migrations.new('master', nil, 'killbill/killbill-analytics-plugin', ENV['TOKEN']).migrations
+      migrations = KPM::Migrations.new('analytics-plugin-3.0.2', nil, 'killbill/killbill-analytics-plugin', ENV['TOKEN']).migrations
       # No migration yet
       migrations.size.should == 0
     end
@@ -12,13 +12,13 @@ describe KPM::Migrations do
     it 'should be able to find migrations for a ruby plugin' do
       migrations = KPM::Migrations.new('master', nil, 'killbill/killbill-cybersource-plugin', ENV['TOKEN']).migrations
       # No migration yet
-      migrations.size.should == 0
+      migrations.size.should == 1
     end
   end
 
   context 'killbill' do
     it 'should be able to find migrations between two versions' do
-      migrations = KPM::Migrations.new('master', 'work-for-release-0.16.4', 'killbill/killbill', ENV['TOKEN']).migrations
+      migrations = KPM::Migrations.new('work-for-release-0.16.3', 'work-for-release-0.16.4', 'killbill/killbill', ENV['TOKEN']).migrations
 
       migrations.size.should == 1
       migrations.first[:name].should == 'V20160324060345__revisit_payment_methods_indexes_509.sql'
