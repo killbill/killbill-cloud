ea7bea1c3eeeb1de877b4551bc7f4ee21ccd5516
Joel Batista <jbatista@wovenware.com>
2017-04-24 15:11:53 -0700

    Issue #71 - statements source changed from string to file

--- kpm/lib/kpm/database.rb
+++ kpm/lib/kpm/database.rb
@@ -1,3 +1,4 @@
+require 'tmpdir'
 
 module KPM
 
@@ -12,8 +13,11 @@ module KPM
       DATABASE = ENV['DATABASE'] || 'killbill'
       USERNAME = ENV['USERNAME'] || 'root'
       PASSWORD = ENV['PASSWORD'] || 'root'
+      COLUMN_NAME_POS = 3
+      
+      STATEMENT_TMP_FILE = Dir.mktmpdir('statement') + File::SEPARATOR + 'statement.sql'
 
-      MYSQL_COMMAND_LINE = "mysql #{DATABASE} --user=#{USERNAME} --password=#{PASSWORD} -e"
+      MYSQL_COMMAND_LINE = "mysql #{DATABASE} --user=#{USERNAME} --password=#{PASSWORD} "
 
       @@mysql_command_line = MYSQL_COMMAND_LINE
       @@username = USERNAME
@@ -34,16 +38,21 @@ module KPM
       end
 
       def set_mysql_command_line
-        @@mysql_command_line = "mysql #{@@database} --user=#{@@username} --password=#{@@password} -e"
+        @@mysql_command_line = "mysql #{@@database} --user=#{@@username} --password=#{@@password} "
       end
 
-      def execute_insert_statement(table_name, query, qty_to_insert, record_id = nil)
+      def execute_insert_statement(table_name, query, qty_to_insert, table_data, record_id = nil)
 
         if not record_id.nil?
           query = "set #{record_id[:variable]}=#{record_id[:value]}; #{query}"
         end
         query = "SET autocommit=0; #{query} COMMIT;"
-        response = `#{@@mysql_command_line} "#{query.gsub('"','\\"')}" 2>&1`
+        
+        File.open(STATEMENT_TMP_FILE,'w') do |s|
+          s.puts query.gsub('"','\\"')
+        end
+          
+        response = `#{@@mysql_command_line} < "#{STATEMENT_TMP_FILE}" 2>&1`
 
         if response.include? 'ERROR'
           raise Interrupt, "Importing table #{table_name}...... \e[91;1m#{response}\e[0m"
@@ -81,7 +90,7 @@ module KPM
           value_data = rows.map{|row| "(#{row})" }.join(",")
 
           statements << {:query => get_insert_statement(table_name,columns_names,value_data, rows.size),
-                                    :qty_to_insert => rows.size, :table_name => table_name}
+                                    :qty_to_insert => rows.size, :table_name => table_name, :table_data => table}
 
         end
 
