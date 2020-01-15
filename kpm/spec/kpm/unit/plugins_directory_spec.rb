cf49bba6a80e7f8b242885133653ae7d2f8cc063
Pierre-Alexandre Meyer <pierre@mouraf.org>
2020-01-15 15:18:38 +0000

    directory: small regex fix
    No behavior change though.
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- kpm/spec/kpm/unit/plugins_directory_spec.rb
+++ kpm/spec/kpm/unit/plugins_directory_spec.rb
@@ -9,6 +9,22 @@ describe KPM::PluginsDirectory do
   end
 
   it 'should lookup plugins' do
+    group_id, artifact_id, packaging, classifier, version, type = KPM::PluginsDirectory.lookup('analytics', false, '0.20.11')
+    group_id.should eq 'org.kill-bill.billing.plugin.java'
+    artifact_id.should eq 'analytics-plugin'
+    packaging.should eq 'jar'
+    classifier.should be_nil
+    version.should eq '6.0.1'
+    type.should eq :java
+
+    group_id, artifact_id, packaging, classifier, version, type = KPM::PluginsDirectory.lookup('analytics', false, '0.20.11-SNAPSHOT')
+    group_id.should eq 'org.kill-bill.billing.plugin.java'
+    artifact_id.should eq 'analytics-plugin'
+    packaging.should eq 'jar'
+    classifier.should be_nil
+    version.should eq '6.0.1'
+    type.should eq :java
+
     group_id, artifact_id, packaging, classifier, version, type = KPM::PluginsDirectory.lookup('analytics', false, '0.20')
     group_id.should eq 'org.kill-bill.billing.plugin.java'
     artifact_id.should eq 'analytics-plugin'
