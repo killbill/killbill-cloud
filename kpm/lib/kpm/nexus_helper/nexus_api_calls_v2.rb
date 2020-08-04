042623d30dfc74bf30edd439b257a427cef25a99
Pierre-Alexandre Meyer <pierre@mouraf.org>
2020-08-04 07:18:57 +0100

    kpm: initial support for GitHub packages
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- kpm/lib/kpm/nexus_helper/nexus_api_calls_v2.rb
+++ kpm/lib/kpm/nexus_helper/nexus_api_calls_v2.rb
@@ -27,18 +27,13 @@ module KPM
 
     # This is an extract and slim down of functions needed from nexus_cli to maintain the response expected by the base_artifact.
     class NexusApiCallsV2
-      PULL_ARTIFACT_ENDPOINT = '/service/local/artifact/maven/redirect'
-      GET_ARTIFACT_INFO_ENDPOINT = '/service/local/artifact/maven/resolve'
-      SEARCH_FOR_ARTIFACT_ENDPOINT = '/service/local/lucene/search'
-
       READ_TIMEOUT_DEFAULT = 60
       OPEN_TIMEOUT_DEFAULT = 60
 
       ERROR_MESSAGE_404 = 'The artifact you requested information for could not be found. Please ensure it exists inside the Nexus.'
 
-      attr_reader :version
-      attr_reader :configuration
-      attr_reader :ssl_verify
+      attr_reader :version, :configuration, :ssl_verify
+
       attr_accessor :logger
 
       def initialize(configuration, ssl_verify, logger)
@@ -49,12 +44,12 @@ module KPM
 
       def search_for_artifacts(coordinates)
         logger.debug "Entered - Search for artifact, coordinates: #{coordinates}"
-        response = get_response(coordinates, SEARCH_FOR_ARTIFACT_ENDPOINT, %i[g a])
+        response = get_response(coordinates, search_for_artifact_endpoint(coordinates), %i[g a])
 
         case response.code
         when '200'
           logger.debug "response body: #{response.body}"
-          return response.body
+          response.body
         when '404'
           raise StandardError, ERROR_MESSAGE_404
         else
@@ -63,47 +58,49 @@ module KPM
       end
 
       def get_artifact_info(coordinates)
-        logger.debug "Entered - Get artifact info, coordinates: #{coordinates}"
-        response = get_response(coordinates, GET_ARTIFACT_INFO_ENDPOINT, nil)
-
-        case response.code
-        when '200'
-          logger.debug "response body: #{response.body}"
-          return response.body
-        when '404'
-          raise StandardError, ERROR_MESSAGE_404
-        else
-          raise UnexpectedStatusCodeException, response.code
-        end
+        get_response_with_retries(coordinates, get_artifact_info_endpoint(coordinates), nil)
       end
 
       def pull_artifact(coordinates, destination)
-        logger.debug "Entered - Pull artifact, coordinates: #{coordinates}"
         file_name = get_file_name(coordinates)
         destination = File.join(File.expand_path(destination || '.'), file_name)
-        logger.debug "destination: #{destination}"
-        response = get_response(coordinates, PULL_ARTIFACT_ENDPOINT, nil)
+        logger.debug { "Downloading to destination: #{destination}" }
 
-        case response.code
-        when '301', '307'
-          location = response['Location'].gsub!(configuration[:url], '')
-          logger.debug 'fetching artifact'
-          file_response = get_response(nil, location, nil)
-
-          File.open(destination, 'wb') do |io|
-            io.write(file_response.body)
-          end
-        when 404
-          raise StandardError, ERROR_MESSAGE_404
-        else
-          raise UnexpectedStatusCodeException, response.code
+        File.open(destination, 'wb') do |io|
+          io.write(get_response_with_retries(coordinates, pull_artifact_endpoint(coordinates), nil))
         end
+
         {
           file_name: file_name,
           file_path: File.expand_path(destination),
           version: version,
           size: File.size(File.expand_path(destination))
         }
+      end
+
+      def pull_artifact_endpoint(_coordinates)
+        '/service/local/artifact/maven/redirect'
+      end
+
+      def get_artifact_info_endpoint(_coordinates)
+        '/service/local/artifact/maven/resolve'
+      end
+
+      def search_for_artifact_endpoint(_coordinates)
+        '/service/local/lucene/search'
+      end
+
+      def build_query_params(coordinates, what_parameters = nil)
+        artifact = parse_coordinates(coordinates)
+        @version = artifact[:version].to_s.upcase
+
+        query = { g: artifact[:group_id], a: artifact[:artifact_id], e: artifact[:extension], v: version, r: configuration[:repository] }
+        query.merge!(c: artifact[:classifier]) unless artifact[:classifier].nil?
+
+        params = what_parameters.nil? ? query : {}
+        what_parameters.each { |key| params[key] = query[key] unless query[key].nil? } unless what_parameters.nil?
+
+        params.map { |key, value| "#{key}=#{value}" }.join('&')
       end
 
       private
@@ -139,17 +136,33 @@ module KPM
         end
       end
 
-      def build_query_params(coordinates, what_parameters = nil)
-        artifact = parse_coordinates(coordinates)
-        @version = artifact[:version].to_s.upcase
+      def get_response_with_retries(coordinates, endpoint, what_parameters)
+        logger.debug { "Fetching coordinates=#{coordinates}, endpoint=#{endpoint}, params=#{what_parameters}" }
+        response = get_response(coordinates, endpoint, what_parameters)
+        logger.debug { "Response body: #{response.body}" }
+        process_response_with_retries(response)
+      end
 
-        query = { g: artifact[:group_id], a: artifact[:artifact_id], e: artifact[:extension], v: version, r: configuration[:repository] }
-        query.merge!(c: artifact[:classifier]) unless artifact[:classifier].nil?
+      def process_response_with_retries(response)
+        case response.code
+        when '200'
+          response.body
+        when '301', '307'
+          location = response['Location']
+          logger.debug { "Following redirect to #{location}" }
 
-        params = what_parameters.nil? ? query : {}
-        what_parameters.each { |key| params[key] = query[key] unless query[key].nil? } unless what_parameters.nil?
-
-        params.map { |key, value| "#{key}=#{value}" }.join('&')
+          new_path = location.gsub!(configuration[:url], '')
+          if new_path.nil?
+            # Redirect to another domain (e.g. CDN)
+            get_raw_response_with_retries(location)
+          else
+            get_response_with_retries(nil, location, nil)
+          end
+        when '404'
+          raise StandardError, ERROR_MESSAGE_404
+        else
+          raise UnexpectedStatusCodeException, response.code
+        end
       end
 
       def get_response(coordinates, endpoint, what_parameters)
@@ -157,11 +170,18 @@ module KPM
         query_params = build_query_params(coordinates, what_parameters) unless coordinates.nil?
         endpoint = endpoint_with_params(endpoint, query_params) unless coordinates.nil?
         request = Net::HTTP::Get.new(endpoint)
+        if configuration.key?(:username) && configuration.key?(:password)
+          request.basic_auth(configuration[:username], configuration[:password])
+        elsif configuration.key?(:token)
+          request['Authorization'] = "token #{configuration[:token]}"
+        end
 
-        logger.debug "request endpoint: #{endpoint}"
+        logger.debug do
+          http.set_debug_output(logger)
+          "HTTP path: #{endpoint}"
+        end
 
-        response = http.request(request)
-        response
+        http.request(request)
       end
 
       def build_http
@@ -171,7 +191,16 @@ module KPM
         http.read_timeout = configuration[:read_timeout] || READ_TIMEOUT_DEFAULT # seconds
         http.use_ssl = (ssl_verify != false)
         http.verify_mode = OpenSSL::SSL::VERIFY_NONE unless ssl_verify
+
+        logger.debug { "HTTP connection: #{http.inspect}" }
+
         http
+      end
+
+      def get_raw_response_with_retries(location)
+        response = Net::HTTP.get_response(URI(location))
+        logger.debug { "Response body: #{response.body}" }
+        process_response_with_retries(response)
       end
 
       def endpoint_with_params(endpoint, query_params)
